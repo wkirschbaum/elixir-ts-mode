dbf3cb421c62ff34fe3a896aab19169f43f02275
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2022-12-18 15:09:08 +0200

    Rename functions

--- elixir-ts-mode.el
+++ elixir-ts-mode.el
@@ -16,19 +16,14 @@
 ;; Code:
 
 (require 'treesit)
-(eval-when-compile
-  (require 'rx))
+(eval-when-compile (require 'rx))
 
-(defgroup elixir nil
-  "Major mode for editing Elixir code."
-  :tag "Elixir"
-  :prefix "elixir-"
-  :group 'languages)
-
-(defcustom elixir-indent-level 2
+(defcustom elixir-ts-mode-indent-offset 2
   "Indentation of Elixir statements."
+  :version "29.1"
   :type 'integer
-  :safe 'integerp)
+  :safe 'integerp
+  :group 'elixir)
 
 ;; Custom faces match highlights.scm as close as possible
 ;; to help with updates
@@ -139,59 +134,174 @@
 
 ;; Faces end
 
-(defconst elixir--definition-keywords
+(defconst elixir-ts-mode--definition-keywords
   '("def" "defdelegate" "defexception" "defguard" "defguardp" "defimpl" "defmacro" "defmacrop" "defmodule" "defn" "defnp" "defoverridable" "defp" "defprotocol" "defstruct"))
 
-(defconst elixir--definition-keywords-re
-  (concat "^" (regexp-opt elixir--definition-keywords) "$"))
+(defconst elixir-ts-mode--definition-keywords-re
+  (concat "^" (regexp-opt elixir-ts-mode--definition-keywords) "$"))
 
-(defconst elixir--definition-module
+(defconst elixir-ts-mode--definition-module
   '("defmodule" "defprotocol"))
 
-(defconst elixir--definition-module-re
-  (concat "^" (regexp-opt elixir--definition-module) "$"))
+(defconst elixir-ts-mode--definition-module-re
+  (concat "^" (regexp-opt elixir-ts-mode--definition-module) "$"))
 
-(defconst elixir--definition-function
+(defconst elixir-ts-mode--definition-function
   '("def" "defp" "defdelegate" "defguard" "defguardp" "defmacro" "defmacrop" "defn" "defnp"))
 
-(defconst elixir--definition-function-re
-  (concat "^" (regexp-opt elixir--definition-function) "$"))
+(defconst elixir-ts-mode--definition-function-re
+  (concat "^" (regexp-opt elixir-ts-mode--definition-function) "$"))
 
-(defconst elixir--kernel-keywords
+(defconst elixir-ts-mode--kernel-keywords
   '("alias" "case" "cond" "else" "for" "if" "import" "quote" "raise" "receive" "require" "reraise" "super" "throw" "try" "unless" "unquote" "unquote_splicing" "use" "with"))
 
-(defconst elixir--kernel-keywords-re
-  (concat "^" (regexp-opt elixir--kernel-keywords) "$"))
+(defconst elixir-ts-mode--kernel-keywords-re
+  (concat "^" (regexp-opt elixir-ts-mode--kernel-keywords) "$"))
 
-(defconst elixir--builtin-keywords
+(defconst elixir-ts-mode--builtin-keywords
   '("__MODULE__" "__DIR__" "__ENV__" "__CALLER__" "__STACKTRACE__"))
 
-(defconst elixir--builtin-keywords-re
-  (concat "^" (regexp-opt elixir--builtin-keywords) "$"))
+(defconst elixir-ts-mode--builtin-keywords-re
+  (concat "^" (regexp-opt elixir-ts-mode--builtin-keywords) "$"))
 
-(defconst elixir--doc-keywords
+(defconst elixir-ts-mode--doc-keywords
   '("moduledoc" "typedoc" "doc"))
 
-(defconst elixir--doc-keywords-re
-  (concat "^" (regexp-opt elixir--doc-keywords) "$"))
+(defconst elixir-ts-mode--doc-keywords-re
+  (concat "^" (regexp-opt elixir-ts-mode--doc-keywords) "$"))
 
-(defconst elixir--reserved-keywords
+(defconst elixir-ts-mode--reserved-keywords
   '("when" "and" "or" "not" "in"
     "not in" "fn" "do" "end" "catch" "rescue" "after" "else"))
 
-(defconst elixir--reserved-keywords
+(defconst elixir-ts-mode--reserved-keywords
   '("when" "and" "or" "not" "in"
     "not in" "fn" "do" "end" "catch" "rescue" "after" "else"))
 
-(defconst elixir--reserved-keywords-re
-  (concat "^" (regexp-opt elixir--reserved-keywords) "$"))
+(defconst elixir-ts-mode--reserved-keywords-re
+  (concat "^" (regexp-opt elixir-ts-mode--reserved-keywords) "$"))
 
-(defconst elixir--reserved-keywords-vector
-  (apply #'vector elixir--reserved-keywords))
+(defconst elixir-ts-mode--reserved-keywords-vector
+  (apply #'vector elixir-ts-mode--reserved-keywords))
+
+(defvar elixir-ts-mode--anonymous-function-end
+  (treesit-query-compile 'elixir '((anonymous_function "end" @end))))
+
+(defvar elixir-ts-mode--operator-parent
+  (treesit-query-compile 'elixir '((binary_operator operator: _ @val))))
+
+(defvar elixir-ts-mode--first-argument
+  (treesit-query-compile
+   'elixir
+   "(arguments . (_) @first-child) (tuple . (_) @first-child)"))
+
+(defvar elixir-ts-mode--syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?| "." table)
+    (modify-syntax-entry ?- "." table)
+    (modify-syntax-entry ?+ "." table)
+    (modify-syntax-entry ?* "." table)
+    (modify-syntax-entry ?/ "." table)
+    (modify-syntax-entry ?< "." table)
+    (modify-syntax-entry ?> "." table)
+    (modify-syntax-entry ?_ "_" table)
+    (modify-syntax-entry ?? "w" table)
+    (modify-syntax-entry ?~ "w" table)
+    (modify-syntax-entry ?! "_" table)
+    (modify-syntax-entry ?' "\"" table)
+    (modify-syntax-entry ?\" "\"" table)
+    (modify-syntax-entry ?# "<" table)
+    (modify-syntax-entry ?\n ">" table)
+    (modify-syntax-entry ?\( "()" table)
+    (modify-syntax-entry ?\) ")(" table)
+    (modify-syntax-entry ?\{ "(}" table)
+    (modify-syntax-entry ?\} "){" table)
+    (modify-syntax-entry ?\[ "(]" table)
+    (modify-syntax-entry ?\] ")[" table)
+    (modify-syntax-entry ?: "'" table)
+    (modify-syntax-entry ?@ "'" table)
+    table)
+  "Syntax table for `elixir-ts-mode.")
+
+(defvar elixir-ts-mode--indent-rules
+  (let ((offset elixir-ts-mode-indent-offset))
+    `((elixir
+       ((parent-is "source") parent-bol 0)
+       ((parent-is "string") parent-bol 0)
+       ;; ensure we don't indent docs by setting no-indent on quoted_content
+       ((parent-is "quoted_content")
+        (lambda (_n parent bol &rest _)
+          (save-excursion
+            (back-to-indentation)
+            (if (bolp)
+                (progn
+                  (goto-char (treesit-node-start parent))
+                  (back-to-indentation)
+                  (point))
+              (point)))) 0)
+       (no-node parent-bol ,offset)
+       ((node-is "|>") parent-bol 0)
+       ((node-is "|") parent-bol 0)
+       ((node-is "}") parent-bol 0)
+       ((node-is ")") (lambda (_node parent &rest _)
+          (if (elixir-ts-mode--indent-parent-bol-p parent)
+              ;; parent-bol
+              (save-excursion
+                (goto-char (treesit-node-start parent))
+                (back-to-indentation)
+                (point))
+
+            ;; grant-parent
+            (treesit-node-start (treesit-node-parent parent))))
+        0)
+       ((node-is "]") parent-bol 0)
+       ((node-is "else_block") elixir-ts-mode--treesit-anchor-grand-parent-bol 0)
+       ((node-is "catch_block") elixir-ts-mode--treesit-anchor-grand-parent-bol 0)
+       ((node-is "rescue_block") elixir-ts-mode--treesit-anchor-grand-parent-bol 0)
+       ((node-is "stab_clause") parent-bol ,offset)
+       ((query ,elixir-ts-mode--operator-parent) grand-parent 0)
+       ((node-is "when") parent 0)
+       ((node-is "keywords") parent-bol ,offset)
+       ((parent-is "body") parent-bol ,offset)
+       ((query ,elixir-ts-mode--first-argument)
+        (lambda (_node parent &rest _)
+          (if (elixir-ts-mode--indent-parent-bol-p parent)
+              ;; parent-bol
+              (save-excursion
+                (goto-char (treesit-node-start parent))
+                (back-to-indentation)
+                (point))
+
+            ;; grant-parent
+            (treesit-node-start (treesit-node-parent parent))))
+          ,offset)
+       ((parent-is "arguments")
+        (lambda (node parent &rest _)
+          ;; grand-parent
+          (treesit-node-start
+           (treesit-node-child parent 0 t)))
+        0)
+       ((parent-is "binary_operator") parent ,offset)
+        ((node-is "pair") first-sibling 0)
+        ((parent-is "tuple") (lambda (_n parent &rest _)
+                               (treesit-node-start
+                                (treesit-node-child parent 0 t))) 0)
+        ((parent-is "list") parent-bol ,offset)
+        ((parent-is "pair") parent ,offset)
+        ((parent-is "map") parent-bol ,offset)
+        ((query ,elixir-ts-mode--anonymous-function-end) parent-bol 0)
+        ((node-is "end") elixir-ts-mode--treesit-anchor-grand-parent-bol 0)
+        ((parent-is "do_block") grand-parent ,offset)
+        ((parent-is "anonymous_function")
+         elixir-ts-mode--treesit-anchor-grand-parent-bol ,offset)
+        ((parent-is "else_block") parent ,offset)
+        ((parent-is "rescue_block") parent ,offset)
+        ((parent-is "catch_block") parent ,offset)
+        ))))
 
 ;; reference:
 ;; https://github.com/elixir-lang/tree-sitter-elixir/blob/main/queries/highlights.scm
-(defvar elixir--treesit-font-lock-settings
+(defvar elixir-ts-mode--font-lock-settings
   (treesit-font-lock-rules
    :language 'elixir
    :feature 'comment
@@ -228,11 +338,11 @@
    :language 'elixir
    :feature 'keyword
    ;; :override `prepend
-   `(,elixir--reserved-keywords-vector @elixir-font-keyword-face
+   `(,elixir-ts-mode--reserved-keywords-vector @elixir-font-keyword-face
                                        ;; these are operators, should we mark them as keywords?
                                        (binary_operator
                                         operator: _ @elixir-font-keyword-face
-                                        (:match ,elixir--reserved-keywords-re @elixir-font-keyword-face)))
+                                        (:match ,elixir-ts-mode--reserved-keywords-re @elixir-font-keyword-face)))
 
    :language 'elixir
    :feature 'doc
@@ -253,12 +363,12 @@
                   (sigil) @elixir-font-comment-doc-face
                   (boolean) @elixir-font-comment-doc-face
                   ]))
-      (:match ,elixir--doc-keywords-re @elixir-font-comment-doc-identifier-face))
+      (:match ,elixir-ts-mode--doc-keywords-re @elixir-font-comment-doc-identifier-face))
      (unary_operator
       operator: "@" @elixir-font-comment-doc-attribute-face
       operand: (call
                 target: (identifier) @elixir-font-comment-doc-identifier-face)
-      (:match ,elixir--doc-keywords-re @elixir-font-comment-doc-identifier-face)))
+      (:match ,elixir-ts-mode--doc-keywords-re @elixir-font-comment-doc-identifier-face)))
 
 
    :language 'elixir
@@ -293,10 +403,10 @@
    :feature 'call
    `((call
       target: (identifier) @elixir-font-keyword-face
-      (:match ,elixir--definition-keywords-re @elixir-font-keyword-face))
+      (:match ,elixir-ts-mode--definition-keywords-re @elixir-font-keyword-face))
      (call
       target: (identifier) @elixir-font-keyword-face
-      (:match ,elixir--kernel-keywords-re @elixir-font-keyword-face))
+      (:match ,elixir-ts-mode--kernel-keywords-re @elixir-font-keyword-face))
      (call
       target: [(identifier) @elixir-font-function-face
                (dot right: (identifier) @elixir-font-function-face)])
@@ -309,20 +419,20 @@
          left: (identifier) @elixir-font-function-face
          operator: "when")
         ])
-      (:match ,elixir--definition-keywords-re @elixir-font-keyword-face))
+      (:match ,elixir-ts-mode--definition-keywords-re @elixir-font-keyword-face))
      (call
       target: (identifier) @elixir-font-keyword-face
       (arguments
        (binary_operator
         operator: "|>"
         right: (identifier) @elixir-font-variable-face))
-      (:match ,elixir--definition-keywords-re @elixir-font-keyword-face)))
+      (:match ,elixir-ts-mode--definition-keywords-re @elixir-font-keyword-face)))
 
    :language 'elixir
    :feature 'constant
    `((binary_operator operator: "|>" right: (identifier) @elixir-font-function-face)
      ((identifier) @elixir-font-constant-builtin-face
-      (:match ,elixir--builtin-keywords-re @elixir-font-constant-builtin-face))
+      (:match ,elixir-ts-mode--builtin-keywords-re @elixir-font-constant-builtin-face))
      ((identifier) @elixir-font-comment-unused-face
       (:match "^_" @elixir-font-comment-unused-face))
      (identifier) @elixir-font-variable-face
@@ -357,144 +467,19 @@
    )
   "Tree-sitter font-lock settings.")
 
-(defvar elixir--anonymous-function-end
-  (treesit-query-compile 'elixir '((anonymous_function "end" @end))))
-
-(defvar elixir--operator-parent
-  (treesit-query-compile 'elixir '((binary_operator operator: _ @val))))
-
-(defvar elixir--first-argument
-  (treesit-query-compile 'elixir "(arguments . (_) @first-child) (tuple . (_) @first-child)"))
-
-(defun elixir--indent-parent-bol-p (parent)
+(defun elixir-ts-mode--indent-parent-bol-p (parent)
   (save-excursion
     (goto-char (treesit-node-start parent))
     (back-to-indentation)
     (and (eq (char-after) ?|) (eq (char-after (1+ (point))) ?>))))
 
-(defun elixir--treesit-anchor-grand-parent-bol (_n parent &rest _)
+(defun elixir-ts-mode--treesit-anchor-grand-parent-bol (_n parent &rest _)
   (save-excursion
     (goto-char (treesit-node-start (treesit-node-parent parent)))
     (back-to-indentation)
     (point)))
 
-(defvar elixir--treesit-indent-rules
-  (let ((offset elixir-indent-level))
-    `((elixir
-       ((parent-is "source") parent-bol 0)
-       ((parent-is "string") parent-bol 0)
-       ;; ensure we don't indent docs by setting no-indent on quoted_content
-       ((parent-is "quoted_content")
-        (lambda (_n parent bol &rest _)
-          (save-excursion
-            (back-to-indentation)
-            (if (bolp)
-                (progn
-                  (goto-char (treesit-node-start parent))
-                  (back-to-indentation)
-                  (point))
-              (point)))) 0)
-       (no-node parent-bol ,offset)
-       ((node-is "|>") parent-bol 0)
-       ((node-is "|") parent-bol 0)
-       ((node-is "}") parent-bol 0)
-       ((node-is ")") (lambda (_node parent &rest _)
-          (if (elixir--indent-parent-bol-p parent)
-              ;; parent-bol
-              (save-excursion
-                (goto-char (treesit-node-start parent))
-                (back-to-indentation)
-                (point))
-
-            ;; grant-parent
-            (treesit-node-start (treesit-node-parent parent))))
-        0)
-       ((node-is "]") parent-bol 0)
-
-       ((node-is "else_block") elixir--treesit-anchor-grand-parent-bol 0)
-       ((node-is "catch_block") elixir--treesit-anchor-grand-parent-bol 0)
-       ((node-is "rescue_block") elixir--treesit-anchor-grand-parent-bol 0)
-       ((node-is "stab_clause") parent-bol ,offset)
-
-       ((query ,elixir--operator-parent) grand-parent 0)
-       ((node-is "when") parent 0)
-
-       ((node-is "keywords") parent-bol ,offset)
-
-       ((parent-is "body") parent-bol ,offset)
-
-       ((query ,elixir--first-argument)
-        (lambda (_node parent &rest _)
-          (if (elixir--indent-parent-bol-p parent)
-              ;; parent-bol
-              (save-excursion
-                (goto-char (treesit-node-start parent))
-                (back-to-indentation)
-                (point))
-
-            ;; grant-parent
-            (treesit-node-start (treesit-node-parent parent))))
-          ,offset)
-
-       ((parent-is "arguments")
-        (lambda (node parent &rest _)
-          ;; grand-parent
-          (treesit-node-start
-           (treesit-node-child parent 0 t)))
-        0)
-
-       ((parent-is "binary_operator") parent ,offset)
-
-        ((node-is "pair") first-sibling 0)
-        ((parent-is "tuple") (lambda (_n parent &rest _)
-                               (treesit-node-start
-                                (treesit-node-child parent 0 t))) 0)
-
-        ((parent-is "list") parent-bol ,offset)
-        ((parent-is "pair") parent ,offset)
-        ((parent-is "map") parent-bol ,offset)
-
-        ((query ,elixir--anonymous-function-end) parent-bol 0)
-
-        ((node-is "end") elixir--treesit-anchor-grand-parent-bol 0)
-
-        ((parent-is "do_block") grand-parent ,offset)
-
-        ((parent-is "anonymous_function") elixir--treesit-anchor-grand-parent-bol ,offset)
-        ((parent-is "else_block") parent ,offset)
-        ((parent-is "rescue_block") parent ,offset)
-        ((parent-is "catch_block") parent ,offset)
-        ))))
-
-(defvar elixir-ts-mode-syntax-table
-  (let ((table (make-syntax-table)))
-    (modify-syntax-entry ?| "." table)
-    (modify-syntax-entry ?- "." table)
-    (modify-syntax-entry ?+ "." table)
-    (modify-syntax-entry ?* "." table)
-    (modify-syntax-entry ?/ "." table)
-    (modify-syntax-entry ?< "." table)
-    (modify-syntax-entry ?> "." table)
-    (modify-syntax-entry ?_ "_" table)
-    (modify-syntax-entry ?? "w" table)
-    (modify-syntax-entry ?~ "w" table)
-    (modify-syntax-entry ?! "_" table)
-    (modify-syntax-entry ?' "\"" table)
-    (modify-syntax-entry ?\" "\"" table)
-    (modify-syntax-entry ?# "<" table)
-    (modify-syntax-entry ?\n ">" table)
-    (modify-syntax-entry ?\( "()" table)
-    (modify-syntax-entry ?\) ")(" table)
-    (modify-syntax-entry ?\{ "(}" table)
-    (modify-syntax-entry ?\} "){" table)
-    (modify-syntax-entry ?\[ "(]" table)
-    (modify-syntax-entry ?\] ")[" table)
-    (modify-syntax-entry ?: "'" table)
-    (modify-syntax-entry ?@ "'" table)
-    table)
-  "Elixir mode syntax table.")
-
-(defvar elixir--treesit-range-rules
+(defvar elixir-ts-mode--treesit-range-rules
   (treesit-range-rules
    :embed 'heex
    :host 'elixir
@@ -506,31 +491,25 @@
 
 ;;;###autoload
 (define-derived-mode elixir-ts-mode prog-mode "Elixir"
+  "Major mode for editing Elixir, powered by tree-sitter."
   :group 'elixir
-  :syntax-table elixir-ts-mode-syntax-table
-  "Major mode for editing Elixir code.
+  :syntax-table elixir-ts-mode--syntax-table
 
-\\{elixir-ts-mode-map}"
-
-  (unless (treesit-ready-p 'elixir)
-    (error "Tree-sitter for Elixir isn't available"))
-
-  (treesit-parser-create 'elixir)
+  (when (treesit-ready-p 'elixir)
+    (treesit-parser-create 'elixir))
 
   ;; Comments
   (setq-local comment-start "# ")
-  (setq-local comment-start-skip "#+\\s-*")
+  (setq-local comment-start-skip (rx "#" (* (syntax whitespace))))
   (setq-local comment-end "")
+  (setq-local comment-end-skip (rx "\n" (* (syntax whitespace))))
 
     ;; Electric.
   (setq-local electric-indent-chars
               (append "]" ")" "}" "\"" "end" electric-indent-chars))
 
-  ;; Treesit-mode.
-  (setq-local treesit-mode-supported t)
-  (setq-local treesit-required-languages '(elixir))
-  (setq-local treesit-simple-indent-rules elixir--treesit-indent-rules)
-  (setq-local treesit-font-lock-settings elixir--treesit-font-lock-settings)
+  ;; Font-lock
+  (setq-local treesit-font-lock-settings elixir-ts-mode--font-lock-settings)
   (setq-local treesit-font-lock-feature-list
               '(( comment string )
                 ( keyword unary-operator operator doc)
@@ -538,11 +517,14 @@
                 ( sigil string-escape)
                 ( string-interpolation )))
 
-  (setq-local treesit-defun-type-regexp (rx (or "do_block")))
+  ;; Imenu
+  ;; (setq-local treesit-imenu-function #'elixir-ts-mode--imenu-treesit-create-index)
 
+  ;; Indent
+  (setq-local indent-tabs-mode nil
+              treesit-simple-indent-rules elixir-ts-mode--indent-rules)
   ;; Navigation
-
-  (setq-local treesit-imenu-function #'elixir--imenu-treesit-create-index)
+  (setq-local treesit-defun-type-regexp (rx (or "do_block")))
 
   (treesit-major-mode-setup))
 
