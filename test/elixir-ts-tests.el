190ad2f0037d2c136413c7094781a839140cc88e
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2022-12-27 22:32:20 +0200

    Add test workflow with temporary .so files
    .so files should not be in this repository, but adding it to
    test the pipeline

---
+++ test/elixir-ts-tests.el
@@ -0,0 +1,17 @@
+(require 'ert)
+(require 'elixir-ts-mode)
+
+(ert-deftest elixir-ts-mode-indentation ()
+  "Test module indentation."
+  (let ((original "defmodule Foo do
+def bar(), do: \"bar\"
+     end")
+        (expected "defmodule Foo do
+  def bar(), do: \"bar\"
+end"))
+  (with-temp-buffer
+    (progn
+      (insert original)
+      (elixir-ts-mode)
+      (indent-region (point-min) (point-max))
+      (should (equal (buffer-string) expected))))))
