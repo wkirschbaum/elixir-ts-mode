8f99ac9af657726f42ffe78c5f31ac4f57832264
Wilhelm H Kirschbaum <wkirschbaum@gmail.com>
2023-01-15 20:37:55 +0200

    Fix map indentation

--- test/elixir-ts-tests.el
+++ test/elixir-ts-tests.el
@@ -4,13 +4,33 @@
 
 (ert-deftest elixir-ts-mode-indentation ()
   "Test module indentation."
-  (skip-unless (treesit-language-available-p 'elixir))
+  (skip-unless (and (treesit-language-available-p 'elixir)
+                    (treesit-language-available-p 'heex)))
   (let ((original "defmodule Foo do
 def bar(), do: \"bar\"
      end")
         (expected "defmodule Foo do
   def bar(), do: \"bar\"
 end"))
+  (with-temp-buffer
+    (progn
+      (insert original)
+      (elixir-ts-mode)
+      (indent-region (point-min) (point-max))
+      (should (equal (buffer-string) expected))))))
+
+(ert-deftest elixir-ts-mode-indentation-map ()
+  "Test module indentation."
+  (skip-unless (and (treesit-language-available-p 'elixir)
+                    (treesit-language-available-p 'heex)))
+  (let ((original "map = %{
+\"a\" => 1,
+\"b\" => 2
+     }")
+        (expected "map = %{
+  \"a\" => 1,
+  \"b\" => 2
+}"))
   (with-temp-buffer
     (progn
       (insert original)
