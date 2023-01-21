ecf43260c02517650632f806291fe4d4bd015e53
Wilhelm H Kirschbaum <wkirschbaum@gmail.com>
2023-01-21 13:47:48 +0200

    Fix indentation for typespec returns

--- elixir-ts-mode.el
+++ elixir-ts-mode.el
@@ -262,21 +262,26 @@
        ((query ,elixir-ts-mode--capture-operator-parent) grand-parent 0)
        ((node-is "^when$") parent 0)
        ((node-is "^keywords$") parent-bol ,offset)
-       ((node-is "^binary_operator$")
-        (lambda (_n parent &rest _)
-          (if (equal (treesit-node-type parent) "do_block")
-              (treesit-node-start (treesit-node-parent parent))
-            (- (treesit-node-start parent) ,offset))) ,offset)
        ((parent-is "^body$") parent-bol ,offset)
+       ((node-is "^binary_operator$")
+        (lambda (node parent &rest _)
+          (cond
+           ((equal (treesit-node-type parent) "do_block")
+            ;; when it is a do_block, we want to find the call
+            ;; to indent to
+            (treesit-node-start (treesit-node-parent parent)))
+           ((equal (treesit-node-type parent) "binary_operator")
+            (treesit-node-start parent))
+           (t (- (treesit-node-start parent) ,offset)))) ,offset)
        ((parent-is "^arguments$") grand-parent ,offset)
        ((parent-is "^binary_operator$")
-        (lambda (_n parent bol &rest _)
-          (save-excursion
-            (progn
+        (lambda (node parent bol &rest _)
+          (if (treesit-node-eq (treesit-node-child-by-field-name parent "right:") node)
+              (treesit-node-start parent)
+            (save-excursion
               (goto-char (treesit-node-start parent))
               (back-to-indentation)
-              (point))
-            (point))) ,offset)
+              (point)))) ,offset)
        ((node-is "^pair$") first-sibling 0)
        ((parent-is "^tuple$") (lambda (_n parent &rest _)
                               (treesit-node-start
