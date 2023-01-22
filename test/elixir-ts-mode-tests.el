53cfb6f6d15c953ec2530c604c57efb9c29317be
Wilhelm H Kirschbaum <wkirschbaum@gmail.com>
2023-01-22 17:33:14 +0200

    Split failing string tests

--- test/elixir-ts-mode-tests.el
+++ test/elixir-ts-mode-tests.el
@@ -6,6 +6,9 @@
   (skip-unless (and (treesit-ready-p 'elixir) (treesit-ready-p 'heex)))
   (ert-test-erts-file (ert-resource-file "indent.erts")))
 
+(ert-deftest elixir-ts-mode-test-string-indentation ()
+  (skip-unless (and (treesit-ready-p 'elixir) (treesit-ready-p 'heex)))
+  (ert-test-erts-file (ert-resource-file "string-indent.erts")))
 
 (provide 'elixir-ts-mode-tests)
 ;;; elixir-ts-mode-tests.el ends here
