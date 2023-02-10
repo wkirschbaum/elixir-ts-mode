e79093164fbd48ab6576094135be4892d01056d1
Wilhelm H Kirschbaum <wkirschbaum@gmail.com>
2023-02-10 13:44:12 +0200

    Fix test run

--- test/elixir-ts-mode-tests.el
+++ test/elixir-ts-mode-tests.el
@@ -6,9 +6,5 @@
   (skip-unless (and (treesit-ready-p 'elixir) (treesit-ready-p 'heex)))
   (ert-test-erts-file (ert-resource-file "indent.erts")))
 
-(ert-deftest elixir-ts-mode-test-known-issues ()
-  (skip-unless (and (treesit-ready-p 'elixir) (treesit-ready-p 'heex)))
-  (ert-test-erts-file (ert-resource-file "known-issues.erts")))
-
 (provide 'elixir-ts-mode-tests)
 ;;; elixir-ts-mode-tests.el ends here
